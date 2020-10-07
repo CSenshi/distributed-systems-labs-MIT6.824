f842edb93be29ba43f2501b51bc0841f8dd7cbeb
CSenshi <saba.pochkhua@gmail.com>
2020-10-07 23:43:19 +0400

    add state raft variables

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -17,9 +17,12 @@ package raft
 //   in the same server.
 //
 
-import "sync"
-import "sync/atomic"
-import "../labrpc"
+import (
+	"sync"
+	"sync/atomic"
+
+	"../labrpc"
+)
 
 // import "bytes"
 // import "../labgob"
@@ -41,6 +44,10 @@ type ApplyMsg struct {
 	CommandIndex int
 }
 
+type LogEntry struct {
+	//	ToDo: Fill
+}
+
 //
 // A Go object implementing a single Raft peer.
 //
@@ -51,9 +58,18 @@ type Raft struct {
 	me        int                 // this peer's index into peers[]
 	dead      int32               // set by Kill()
 
-	// Your data here (2A, 2B, 2C).
-	// Look at the paper's Figure 2 for a description of what
-	// state a Raft server must maintain.
+	/* Persistent state on all servers (Updated on stable storage before responding to RPCs) */
+	currentTerm int        // latest term server has seen (initialized to 0 on first boot, increases monotonically)
+	votedFor    int        // candidateID that received vote in current term (or null if none)
+	log         []LogEntry // log entries; each entry contains command for state machine, and term when entry was received by leader (first index is 1)
+
+	/* Volatile state on all servers */
+	commitIndex int // index of highest log entry known to be committed (initialized to 0, increases monotonically)
+	lastApplied int // index of highest log entry applied to state machine (initialized to 0, increases monotonically)
+
+	/* Volatile state on leaders (Reinitialized after election) */
+	nextIndex  []int // for each server, index of the next log entry to send to that server (initialized to leader last log index + 1)
+	matchIndex []int //for each server, index of highest log entry known to be replicated on server (initialized to 0, increases monotonically)
 
 }
 
@@ -112,7 +128,7 @@ func (rf *Raft) readPersist(data []byte) {
 type RequestVoteArgs struct {
 	// Your data here (2A, 2B).
 	Term         int // candidate’s term
-	CandidateId  int // candidate requesting vote
+	CandidateID  int // candidate requesting vote
 	LastLogIndex int // index of candidate’s last log entry (§5.4)
 	LastLogTerm  int // term of candidate’s last log entry (§5.4)
 }
@@ -232,6 +248,18 @@ func Make(peers []*labrpc.ClientEnd, me int,
 	rf.me = me
 
 	// Your initialization code here (2A, 2B, 2C).
+	/* Persistent state on all servers */
+	rf.currentTerm = 0
+	rf.votedFor = -1
+	rf.log = make([]LogEntry, 0)
+
+	/* Volatile state on all servers */
+	rf.commitIndex = 0
+	rf.lastApplied = -1
+
+	/* Volatile state on leaders */
+	rf.nextIndex = nil
+	rf.matchIndex = nil
 
 	// initialize from state persisted before a crash
 	rf.readPersist(persister.ReadRaftState())
